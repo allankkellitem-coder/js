5e82b7aff633578941e119d4b92e758be38ca1c8
ALLAN <allankkellitem@gmail.com>
2026-02-23 15:46:31 +0200

    change 1

--- operators.js
+++ operators.js
@@ -19,6 +19,6 @@ console.log(20 >=20); //true
  * Discuss different variable naming conventions (camelCase, snake_case, PascalCase)
  * what are keywords in a programming language? give examples of keywords in JavaScript
  * What is the difference beteween == and === in JavaScript? give examples
- * What does the modulus operator (%) do? give examples
+ * What does the modulus operator (%) do in aritmetic give examples
  * What is the difference between let and const in JavaScript? give examples
  */
